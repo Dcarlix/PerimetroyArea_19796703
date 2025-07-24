d99d3f432b99b2b28ad22c054281d0f07153e8dd
Carlos <carlitosxd2006@gmail.com>
2025-07-23 20:47:50 -0600

    subiendo el proyecto

---
+++ PerimetroyArea19796703/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PerimetroyArea19796703
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PerimetroyArea19796703/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace PerimetroyArea19796703
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+		private void Button_Clicked(object sender, EventArgs e)
+		{
+            if (double.TryParse(Altura.Text, out double alt) && double.TryParse(Base.Text, out double basee) && double.TryParse(Lado2.Text, out double l2) && double.TryParse(Lado3.Text, out double l3))
+            {
+                double area = (alt * basee)/ 2;
+
+                double perimetro = basee + l2 + l3;
+
+                ResultArea.Text = area.ToString();
+                ResultPerimetro.Text = perimetro.ToString();
+            }
+            else
+            {
+                DisplayAlert("Error", "Solo numeros y no dejar en vacio", "Ok");
+            }
+        }
+    }
+
+}
